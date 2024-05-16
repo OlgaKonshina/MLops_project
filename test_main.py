29c0c0b8b54d733633e0d3851d1e34d416c13bbe
Olga <konshinaolga32@gmail.com>
2024-05-16 17:22:25 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,12 +3,12 @@ from streamlit.testing.v1 import AppTest
 
 # проверка запуска приложения
 def test_start_app():
-    at = AppTest.from_file("src/main.py", default_timeout=30).run()
+    at = AppTest.from_file("main.py", default_timeout=30).run()
     assert not at.exception
 
 
 def test_button_app():
-    at = AppTest.from_file("src/main.py").run()
+    at = AppTest.from_file("main.py").run()
     at.button[0].click().run()
     at.button[1].click().run()
     assert at.button[0].label == "Добавить изображение в коллекцию из файла"
