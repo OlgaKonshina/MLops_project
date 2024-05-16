f9dd6e29dad32bf2cfe706e5360b73204b839629
Olga <konshinaolga32@gmail.com>
2024-05-16 14:35:55 +0300

    Add files via upload

---
+++ test_main.py
@@ -0,0 +1,16 @@
+from streamlit.testing.v1 import AppTest
+
+
+# проверка запуска приложения
+def test_start_app():
+    at = AppTest.from_file("src/main.py", default_timeout=30).run()
+    assert not at.exception
+
+
+def test_button_app():
+    at = AppTest.from_file("src/main.py").run()
+    at.button[0].click().run()
+    at.button[1].click().run()
+    assert at.button[0].label == "Добавить изображение в коллекцию из файла"
+    assert at.button[1].label == "Проанализировать изображения"
+    assert not at.exception
